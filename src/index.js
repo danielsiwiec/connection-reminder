da3c01c5e11d4555ca5f3bc57928eff09ebc12b6
Dan Siwiec <daniel.siwiec@gmail.com>
2018-02-08 01:31:02 -0800

    Add a single page

--- src/index.js
+++ src/index.js
@@ -4,6 +4,4 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/hello', (req, res) => res.json({hello: 'foo'}))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
