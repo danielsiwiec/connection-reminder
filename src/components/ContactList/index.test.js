d725d009a4cf85d33d6edbd028fd2a5d8a317b0a
Dan Siwiec <daniel.siwiec@gmail.com>
2018-02-26 22:58:34 -0800

    Fix front end tests

--- src/components/ContactList/index.test.js
+++ src/components/ContactList/index.test.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import { shallow } from 'enzyme';
-import Home from './';
+import React from 'react'
+import { shallow } from 'enzyme'
+import ContactList from './'
 
-it('Test example', () => {
-  const wrapper = shallow(<Home />);
-  expect(wrapper.is('section')).toBeTruthy();
-});
+describe('ContactList', () => {
+  it('should render list items', () => {
+    let contacts = [{name: 'Bob'}, {name: 'Joe'}]
+    const wrapper = shallow(<ContactList contacts={contacts} />)
+    expect(wrapper.find('li').length).toBe(2)
+    expect(wrapper.find('ul').childAt(0).text()).toContain('Bob')
+    expect(wrapper.find('ul').childAt(1).text()).toContain('Joe')
+  })
+
+  it('should be fine with empty list', () => {
+    const wrapper = shallow(<ContactList contacts={[]} />)
+    expect(wrapper.find('li').length).toBe(0)
+  })
+})
+
