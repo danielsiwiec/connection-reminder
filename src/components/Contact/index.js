39a1d4b34975e72f26776465415d499bb80863d9
Dan Siwiec <daniel.siwiec@gmail.com>
2018-03-02 00:25:45 -0800

    Add meterial icons

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,6 +4,10 @@ import Button from 'material-ui/Button'
 import Chip from 'material-ui/Chip'
 import TableRow from 'material-ui/Table/TableRow'
 import TableCell from 'material-ui/Table/TableCell'
+import Done from 'material-ui-icons/Done'
+import Delete from 'material-ui-icons/Delete'
+import WatchLater from 'material-ui-icons/WatchLater'
+
 
 class Contact extends Component {
   render() {
@@ -16,9 +20,9 @@ class Contact extends Component {
           }
         </TableCell>
         <TableCell>
-          <Button color='primary' onClick={() => this.props.check(this.props.index)}>Check</Button>
-          <Button onClick={() => this.props.bump(this.props.index)}>Bump</Button>
-          <Button color='secondary' onClick={() => this.props.remove(this.props.index)}>Remove</Button>
+          <Button color='primary' onClick={() => this.props.check(this.props.index)}><Done /></Button>
+          <Button onClick={() => this.props.bump(this.props.index)}><WatchLater /></Button>
+          <Button color='secondary' onClick={() => this.props.remove(this.props.index)}><Delete /></Button>
         </TableCell>
       </TableRow>
     )
