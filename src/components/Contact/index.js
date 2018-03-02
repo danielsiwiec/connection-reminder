6f911f71a874f4d8d6b839161c3bc0dbf6eb9365
Dan Siwiec <daniel.siwiec@gmail.com>
2018-03-02 02:44:36 -0800

    Fix icon colors

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -2,9 +2,9 @@ import React, {Component} from 'react'
 import moment from 'moment'
 import IconButton from 'material-ui/IconButton'
 import Chip from 'material-ui/Chip'
-import Done from 'material-ui-icons/Done'
-import Delete from 'material-ui-icons/Delete'
-import WatchLater from 'material-ui-icons/WatchLater'
+import DoneIcon from 'material-ui-icons/Done'
+import DeleteIcon from 'material-ui-icons/Delete'
+import WatchLaterIcon from 'material-ui-icons/WatchLater'
 import {ListItem, ListItemIcon, ListItemText} from 'material-ui/List'
 
 class Contact extends Component {
@@ -17,13 +17,13 @@ class Contact extends Component {
         {this.props.contact.tags &&
         <ListItem>{this.renderTags()}</ListItem>}
         <ListItemIcon>
-          <IconButton color='primary' onClick={() => this.props.check(this.props.index)}><Done /></IconButton>
+          <IconButton onClick={() => this.props.check(this.props.index)}><DoneIcon color='primary' /></IconButton>
         </ListItemIcon>
         <ListItemIcon>
-          <IconButton onClick={() => this.props.bump(this.props.index)}><WatchLater /></IconButton>
+          <IconButton onClick={() => this.props.bump(this.props.index)}><WatchLaterIcon /></IconButton>
         </ListItemIcon>
         <ListItemIcon>
-          <IconButton color='secondary' onClick={() => this.props.remove(this.props.index)}><Delete /></IconButton>
+          <IconButton onClick={() => this.props.remove(this.props.index)}><DeleteIcon color='secondary'/></IconButton>
         </ListItemIcon>
       </ListItem>
     )
